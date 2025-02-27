0384e9d47f18b6874543ddcfb87b66a07437a58f
Luis Carlos Pando <hey@luiscarlospando.com>
2025-02-27 16:55:11 -0600

    Agregar botones de modales

--- index.php
+++ index.php
@@ -316,7 +316,7 @@
                     data-aos-delay="500"
                 >
                     <li class="list-inline-item mb-3 mb-xl-0">
-                        <a href="#" class="btn btn-secondary btn-lg"
+                        <a href="javascript:void(0)" class="btn btn-secondary btn-lg"
                            data-content-type="text"
                            data-text-content="<h4>Requisitos Persona Física</h4>
                                              <ul>
@@ -331,7 +331,7 @@
                         </a>
                     </li>
                     <li class="list-inline-item mb-3 mb-xl-0">
-                        <a href="#" class="btn btn-primary btn-lg"
+                        <a href="javascript:void(0)" class="btn btn-primary btn-lg"
                            data-content-type="text"
                            data-text-content="<h4>Requisitos Persona Moral</h4>
                                              <ul>
