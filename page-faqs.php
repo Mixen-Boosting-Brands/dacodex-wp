9d871e76fa11315ec4054c25819a017e09b91a76
Luis Carlos Pando <hey@luiscarlospando.com>
2025-02-27 16:12:25 -0600

    Armar FAQs

---
+++ page-faqs.php
@@ -0,0 +1,247 @@
+<?php
+/*
+Template Name: FAQs
+*/
+get_header(); ?>
+
+<section id="jumbotron" class="bg-dark jumbotron-interna">
+    <div
+        class="top-bar"
+        data-aos="fade-in"
+        data-aos-duration="1000"
+        data-aos-delay="0"
+    >
+        <ul class="list-inline mb-0">
+            <li class="list-inline-item">
+                <a href="https://wa.link/byytp0" target="_blank"">
+                    <i class="fa-brands fa-whatsapp"></i> +52 (614)
+                    247-6164
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="tel:+526144789000">
+                    <i class="fa-solid fa-phone"></i> +52 (614) 478-9000
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="mailto:ventas@dacodex.com">
+                    <i class="fa-solid fa-envelope"></i>
+                    ventas@dacodex.com
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="overlay"></div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 my-auto">
+                <h1 data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay="100">Aquí resolvemos tus dudas</h1>
+                <p data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay="200">Solicita más información de nuestros espacios disponibles.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="py-60">
+    <div class="container">
+        <div class="row mb-4">
+            <div class="col-12 text-center">
+                <h2 data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay="100">
+                    Preguntas Frecuentes
+                </h2>
+
+                <p data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+                    Renta de oficinas privadas, estaciones cowork y oficinas virtuales.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="accordion accordion-flush" id="acordion-faqs" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                ¿Cuáles son los  requisitos para hacer la contratación?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#acordion-faqs">
+                            <div class="accordion-body">
+                                <p><strong>Para Personas Físicas con o sin actividad empresarial copia de:</strong></p>
+                                <ul>
+                                    <li>Identificación oficial.</li>
+                                    <li>Comprobante de domicilio.</li>
+                                    <li>Constancia de Situación Fiscal.</li>
+                                    <li>Carátula bancaria (sin cantidades).</li>
+                                    <li>Depósito de renta.</li>
+                                </ul>
+
+                                <p><strong>Para las Personas Morales copia de:</strong></p>
+                                <ul>
+                                    <li>Acta constitutiva.</li>
+                                    <li>Poderes del representante legal.</li>
+                                    <li>Identificación oficial del representante legal.</li>
+                                    <li>Comprobante de domicilio del representante legal.</li>
+                                    <li>Constancia de Situación Fiscal.</li>
+                                    <li>Depósito de renta.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                ¿Las oficinas están disponibles para ocupación inmediata?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#acordion-faqs">
+                            <div class="accordion-body">
+                                <p>Si, una vez que tengamos el contrato firmado y el depósito realizado puedes ocupar inmediatamente la oficina rentada.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                                ¿Qué servicio de internet utilizan? ¿Está incluido en la tarifa mensual?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#acordion-faqs">
+                            <div class="accordion-body">
+                                <p>Los Centros de Negocios Dacodex cuentas con internet simétrico de alta velocidad incluido en:</p>
+                                <ul>
+                                    <li>Oficinas físicas</li>
+                                    <li>Coworker</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+                                ¿Puedo contar con un lugar fijo para trabajar?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseFour" class="accordion-collapse collapse" data-bs-parent="#acordion-faqs">
+                            <div class="accordion-body">
+                                <p>Al contratar una oficina física o un espacio coworker en los centros de negocios Dacodex cuentas con un lugar propio y fijo para trabajar.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+                                ¿Tendré servicios como los de una secretaria propia?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseFive" class="accordion-collapse collapse" data-bs-parent="#acordion-faqs">
+                            <div class="accordion-body">
+                                <p>Nuestros asistentes en recepción contestan las llamadas para tu empresa y reciben a tus visitantes y correspondencia.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
+                                ¿Las oficinas privadas, coworker y virtuales funcionan 24 horas los 365 días del año?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseSix" class="accordion-collapse collapse" data-bs-parent="#acordion-faqs">
+                            <div class="accordion-body">
+                                <p>Así es, sin embargo el servicio de recepción solo funciona de <strong>09:00 hrs a 17:00 hrs</strong> de <strong>Lunes a Viernes</strong>.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseEleven" aria-expanded="false" aria-controls="flush-collapseEleven">
+                                ¿La contestación de la línea compartida y personalizada es por medio de una grabación?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseEleven" class="accordion-collapse collapse" data-bs-parent="#acordion-faqs">
+                            <div class="accordion-body">
+                                <p>No, en los Business Centers Dacodex la contestación la realiza una asistente bilingüe en persona.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
+                                ¿Para cuántas personas son sus salas de juntas?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseSeven" class="accordion-collapse collapse" data-bs-parent="#acordion-faqs">
+                            <div class="accordion-body">
+                                <p>Contamos con salas de juntas desde 4 y hasta 12 personas.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseEight" aria-expanded="false" aria-controls="flush-collapseEight">
+                                ¿Manejan el servicio de Coffe break o catering?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseEight" class="accordion-collapse collapse" data-bs-parent="#acordion-faqs">
+                            <div class="accordion-body">
+                                <p>Si. El servicio se cotiza según sus requerimientos.</p>
+                                <p class="alert alert-warning"><i class="fa-solid fa-circle-exclamation"></i> No se permite introducir bebidas y alimentos a las salas de juntas y/o capacitación.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseNine" aria-expanded="false" aria-controls="flush-collapseNine">
+                                ¿El uso de pantalla ya viene incluido en el costo de la sala de juntas?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseNine" class="accordion-collapse collapse" data-bs-parent="#acordion-faqs">
+                            <div class="accordion-body">
+                                <p>Si.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTen" aria-expanded="false" aria-controls="flush-collapseTen">
+                                ¿Cómo me informan de la correspondencia que llega?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseTen" class="accordion-collapse collapse" data-bs-parent="#acordion-faqs">
+                            <div class="accordion-body">
+                                <p>Se le notifica por medio de mensaje de WhatsApp el día que se recibió la correspondencia.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseEleven" aria-expanded="false" aria-controls="flush-collapseEleven">
+                                Si la correspondencia que llego me es urgente revisarla ¿Cómo me apoyan?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseEleven" class="accordion-collapse collapse" data-bs-parent="#acordion-faqs">
+                            <div class="accordion-body">
+                                <p>Tenemos dos opciones:</p>
+                                <ul>
+                                    <li>Se puede digitalizar el documento</li>
+                                    <li>Se le puede hacer llegar con un propio</li>
+                                </ul>
+                                <p><em>Este servicio tiene un costo adicional que se cotiza al momento de su requerimiento.</em></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
