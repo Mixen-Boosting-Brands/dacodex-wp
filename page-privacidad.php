7345b1cc54ccc2eb5b576a6aaaa847666bbcd9cb
Luis Carlos Pando <hey@luiscarlospando.com>
2025-02-27 15:49:00 -0600

    Crear vista privacidad

---
+++ page-privacidad.php
@@ -0,0 +1,131 @@
+<?php
+/*
+Template Name: Privacidad
+*/
+get_header(); ?>
+
+<section id="jumbotron" class="bg-dark jumbotron-interna">
+    <div
+        class="top-bar"
+        data-aos="fade-in"
+        data-aos-duration="1000"
+        data-aos-delay="0"
+    >
+        <ul class="list-inline mb-0">
+            <li class="list-inline-item">
+                <a href="https://wa.link/byytp0" target="_blank"">
+                    <i class="fa-brands fa-whatsapp"></i> +52 (614)
+                    247-6164
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="tel:+526144789000">
+                    <i class="fa-solid fa-phone"></i> +52 (614) 478-9000
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a href="mailto:ventas@dacodex.mx">
+                    <i class="fa-solid fa-envelope"></i>
+                    ventas@dacodex.mx
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="overlay"></div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 my-auto">
+                <h2 data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay="100">Aviso de Privacidad</h2>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="descanso descanso-1 pt-60 pb-30">
+    <div class="container">
+        <div class="row">
+            <div
+                class="col-12"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay="300"
+            >
+                <h2 class="text-uppercase">Aviso de privacidad</h2>
+
+                <h3 class="text-uppercase">I. Identidad y domicilio del responsable que trata los datos personales.</h3>
+                <p>Tierra Buena Inversiones S.A. de C.V. (en lo sucesivo "DACODEX"), con domicilio en Av. Pedro Zuloaga #11602, Colonia Labor de Terrazas, Chihuahua, Chihuahua., C.P. 31223 es el responsable del tratamiento y protección de tus datos personales. Hace de tu conocimiento que la información de nuestros clientes es tratada de forma estrictamente confidencial y al respecto señalamos lo siguiente:</p>
+
+                <h3 class="text-uppercase">II. Datos personales que serán sometidos a tratamiento.</h3>
+                <p>DACODEX utilizara los siguientes datos personales de forma directa, a través de nuestros sitios de Internet, servicios en línea o por cualquier otra fuente que está permitida por la Ley Federal de Protección de Datos Personales en Posesiones de los Particulares (en lo sucesivo la "Ley"):</p>
+
+                <p>Datos de identificación: nombre completo, dirección, teléfono de casa, celular y/o de trabajo, firma, RFC, CURP, lugar y fecha de nacimiento y edad.</p>
+                <p>Datos laborales: ocupación, nombre de la empresa o dependencia, puesto, área o departamento, domicilio, teléfono, correo electrónico de trabajo, referencias laborales y referencias personales.</p>
+                <p>Datos Patrimoniales: historial crediticio, cuentas bancarias, cualquier tipo de garantía otorgada, y servicios contratados.</p>
+
+                <p>Conforme a lo anterior y conforme a lo establecido por la Ley Federal de Protección de Datos Personales en Posesión de los Particulares (la "Ley") y su Reglamento, DACODEX no tratará datos personales sensibles de tu persona.</p>
+
+                <h3 class="text-uppercase">III. Finalidades del tratamiento.</h3>
+                <p>DACODEX utilizará tus Datos Personales para las siguientes finalidades:</p>
+
+                <h4>A. Originarias.</h4>
+                <p>DACODEX tratará los datos personales con la finalidad de llevar a cabo las actividades y gestiones enfocadas al cumplimiento de las obligaciones originadas y derivadas de cualquier relación jurídica y comercial que establezcamos con motivo de la prestación de nuestros servicios; facturación; cobranza; servicio técnico y proporcionar, renovar, cambiar o cancelar los servicios que nos solicita, para el cumplimiento de leyes, reglamentos y disposiciones legales aplicables.</p>
+
+                <h4>B. Secundarias.</h4>
+                <p>Asimismo, DACODEX tratará datos personales para otras finalidades como enviar ofertas, avisos y/o mensajes promocionales; comunicaciones con fines de mercadotecnia, publicitarios o telemarketing sobre productos y servicios nuevos o existentes; realizar encuestas; estadísticas; estudios de mercado; participar en redes sociales, chats y/o foros de discusión; participar en eventos, trivias y concursos, evaluar la calidad de los servicios; y en general para cualquier actividad encaminada a promover, mantener, mejorar y evaluar nuestros servicios.</p>
+
+                <h3 class="text-uppercase">IV. Mecanismos para manifestar a dacodex la negativa al tratamiento de tus datos personales para la realización de finalidades secundarias.</h3>
+                <p>En caso de que no desees que tus Datos Personales sean tratados para las finalidades secundarias referidas en el párrafo III inmediato anterior, envía comunicación por escrito a la dirección de correo electrónico <a href="mailto:gerente@dacodex.mx">gerente@dacodex.mx</a> en la que solicites que DACODEX te facilite los formatos mediante los cuales podrás formalizar dicha negativa.</p>
+                <p>Dicha negativa para el uso de tus Datos Personales no será motivo para que DACODEX te niegue los servicios y beneficios de la empresa.</p>
+
+                <h3 class="text-uppercase">V. Medios para el ejercer los derechos arco.</h3>
+                <p>Tienes el derecho de: (i) acceder a tus Datos Personales que se encuentran en poder de DACODEX y conocer los detalles del tratamiento de los mismos, (ii) rectificarlos en caso de ser inexactos o incompletos, (iii) cancelarlos cuando consideres que no se requieren para alguna de las finalidades señaladas en el presente Aviso de Privacidad y estén siendo utilizados para finalidades no consentidas, o (iv) oponerte al tratamiento de los mismos para fines específicos, según lo establece la Ley.</p>
+
+                <p>Para el ejercicio de tus Derechos ARCO, el Oficial de Protección de Datos Personales pone a disposición la Solicitud ARCO a través de la página de internet www.dacodex.mx o bien mediante solicitud vía correo electrónico a la dirección <a href="mailto:gerente@dacodex.mx">gerente@dacodex.mx</a>. En dicha solicitud podrás encontrar los mecanismos a seguir para acreditar tu identidad o la de tu representante legal, así como la información y documentación que deberás acompañar a dicha solicitud.</p>
+
+                <p>El procedimiento y requisitos para el ejercicio de estos derechos es el siguiente: el Oficial de Protección de Datos recibirá la Solicitud ARCO y dará respuesta con los motivos de su decisión mediante un correo electrónico en un plazo máximo de 20 (veinte) días hábiles contados desde el día en que se haya recibido tu Solicitud ARCO. En caso de que la Solicitud ARCO se conteste de manera afirmativa o procedente, los cambios solicitados se harán en un plazo máximo de 15 días hábiles siguientes a la fecha en que se haya comunicado la respuesta. El Oficial de Protección de Datos podrá notificarte dentro de los plazos referidos en este párrafo la prórroga de los mismos, por una sola vez, por un periodo igual al original.</p>
+
+                <p>DACODEX, a través del Oficial de Protección de Datos, podrá negarte el acceso para que ejerzas tus Derechos ARCO en los supuestos que lo permita la Ley, por lo que deberá informarte el motivo de tal decisión por los medios y en los plazos señalados en el párrafo inmediato anterior. La negativa podrá ser parcial, en cuyo caso DACODEX efectuará el acceso, rectificación, cancelación u oposición en la parte procedente.</p>
+
+                <p>El ejercicio de los Derechos ARCO será gratuito, pero si reiteras tu solicitud en un periodo menor a doce meses, los costos serán los correspondientes de 3 (tres) días de Salario Mínimo General Vigente en el Distrito Federal, más el Impuesto al Valor Agregado, a menos que existan modificaciones sustanciales al Aviso de Privacidad, que motiven nuevas Solicitudes ARCO. Así mismo, deberás de cubrir los gastos justificados de envío o el costo de reproducción en copias u otros formatos y, en su caso, el costo de la certificación de documentos.</p>
+
+                <p>Los datos de contacto de la persona que dará tramite a las solicitudes para el ejercicio de los Derechos ARCO y la solicitud para limitar el uso o divulgación de tus Datos Personales, así como atender cualquier duda que pudiera tener respecto al tratamiento de la información son los siguientes: Oficial de Protección de Datos, número telefónico (614) 247 61 64, correo electrónico <a href="mailto:gerente@dacodex.mx">gerente@dacodex.mx</a>.</p>
+
+                <p>Formato pdf para ejercer los derechos Arco.</p>
+
+                <h3 class="text-uppercase">VI. Medios para revocar el consentimiento para el tratamiento de datos personales.</h3>
+                <p>Podrás revocar el consentimiento para el tratamiento de tus Datos Personales. Sin embargo, es importante que tengas en cuenta que no en todos los casos podremos atender de conformidad tu solicitud, ya que es posible que por alguna obligación legal requiramos seguir tratando tus Datos Personales. Asimismo, deberás considerar que para ciertos fines, la revocación de tu consentimiento implicará que no podamos seguirte prestando alguno de nuestros servicios.</p>
+
+                <p>Para solicitar la revocación del consentimiento deberás enviar un correo electrónico a la dirección <a href="mailto:gerente@dacodex.mx">gerente@dacodex.mx</a> la solicitud para la revocación del consentimiento, misma en la que encontrarás los mecanismos a seguir para acreditar tu identidad o la de tu representante legal, así como la información y documentación que deberás acompañar a dicha solicitud.</p>
+
+                <p>El Oficial de Protección de Datos recibirá la solicitud de revocación del consentimiento y dará respuesta en un plazo máximo de 20 (veinte) días contados desde la fecha en que se recibió la citada solicitud. Si ésta resulta procedente, se hará efectiva la misma dentro de los 15 (quince) días siguientes a la fecha en que se te comunique la respuesta vía correo electrónico. Los plazos antes referidos podrán ser ampliados una sola vez por un periodo igual, siempre y cuando así lo justifiquen las circunstancias del caso.</p>
+
+                <h3 class="text-uppercase">VII. Medios para limitar el uso o divulgación de tus datos personales.</h3>
+                <p>Podrás limitar el uso y divulgación de tu información personal para lo cual DACODEX ofrece los siguientes medios: (i) Requerir la inscripción en el Registro Público para Evitar Publicidad (REPEP), que está a cargo de la Procuraduría Federal del Consumidor, con la finalidad de que tus datos personales no sean utilizados para recibir publicidad o promociones de empresas de bienes o servicios. Para mayor información sobre este Registro, puedes consultar el portal de Internet de la PROFECO <a href="http://repep.profeco.gob.mx/index.jsp" target="_blank">http://repep.profeco.gob.mx/index.jsp</a>, o bien ponerte en contacto directo con ésta; (ii) Requerir vía correo electrónico a la siguiente dirección <a href="mailto:gerente@dacodex.mx">gerente@dacodex.mx</a> la Solicitud para Limitar Uso o Divulgación de Datos Personales, o bien. En dicha solicitud podrás encontrar los mecanismos a seguir para acreditar tu identidad o la de tu representante legal, así como la información y documentación que deberás acompañar a dicha solicitud.</p>
+
+                <p>Todas las solicitudes serán analizadas para resolver sobre su procedencia o no. La resolución será enviada a tu atención, a entera discreción del responsable, mediante correo electrónico o al domicilio indicado en la Solicitud para Limitar Uso o Divulgación de Datos Personales dentro del término de 20 (veinte) días hábiles siguientes contados a partir de la fecha de recepción de la Solicitud. De ser procedente, dicha resolución será efectiva dentro de los 15 (quince) días hábiles siguientes a la fecha en que sea comunicada al titular. Los plazos anteriores podrán ampliarse por un período igual, según sea el caso, cuando las circunstancias así lo justifiquen.</p>
+
+                <h3 class="text-uppercase">VIII. Transferencia de datos personales</h3>
+                <p>Tus datos personales podrán ser transferidos y tratados dentro y fuera del país por personas distintas a DACODEX. En este sentido, tu información puede ser transferida (remitida) a nuestras sociedades controladoras, afiliadas y proveedores a efecto de dar cumplimiento con las finalidades señaladas en este Aviso de Privacidad.</p>
+
+                <h3 class="text-uppercase">IX. Cambios en el aviso de privacidad.</h3>
+                <p>El presente Aviso de Privacidad puede sufrir modificaciones, cambios o actualizaciones derivadas de nuevos requerimientos legales, de nuestras propias necesidades, y de nuestras prácticas de privacidad.</p>
+
+                <p>DACODEX se compromete a mantenerte informado sobre los cambios que pueda sufrir el presente Aviso de Privacidad, a través de uno o varios de los siguientes medios:</p>
+
+                <p>Anuncios visibles en nuestras oficinas ubicadas en Av. Pedro Zuloaga #11602, Colonia Labor de Terrazas, Chihuahua, Chihuahua., C.P. 31223</p>
+
+                <p>Trípticos o folletos disponibles en nuestras oficinas ubicadas en la dirección citada en el numeral anterior.</p>
+
+                <p>En la página de internet www.dacodex.mx Sección "Aviso de Privacidad".</p>
+
+                <p>Vía telefónica al último número de casa, oficina o celular que nuestros clientes nos hayan proporcionado.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
